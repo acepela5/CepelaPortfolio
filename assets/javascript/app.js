53f9fafa8215e03af28eb84f0df6efd05d8480e5
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-06-21 19:02:23 -0500

    side-nav added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     $('.parallax').parallax();
 
-    
+    $('.slider').slider();
+
+    $('.sidenav').sidenav();
+
+    var button = $("#navbtn");
+    var instance = M.Sidenav.getInstance(button);
+    $("#navbtn").on("click", instance.open());
+    instance.open();
   });
