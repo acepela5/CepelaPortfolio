28aa43883e5a99e0be18edb541ad7fcc7e3f420b
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-06-25 13:36:40 -0500

    About page filled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,57 @@
 $(document).ready(function(){
+
     $('.parallax').parallax();
 
     $('.slider').slider();
 
     $('.sidenav').sidenav();
 
-    // var button = $("#navbtn");
-    // var instance = M.Sidenav.getInstance(button);
+    $('.materialboxed').materialbox();
 
-    // $(".sidenav-trigger").on("click", instance.open());
-    // instance.open();
+    var firebaseConfig = {
+        apiKey: "AIzaSyA9mlENUrhKwjTIuLQ3Xcn32sXBIV2kdgA",
+        authDomain: "portfolio-email-1621c.firebaseapp.com",
+        databaseURL: "https://portfolio-email-1621c.firebaseio.com",
+        projectId: "portfolio-email-1621c",
+        storageBucket: "",
+        messagingSenderId: "899266849962",
+        appId: "1:899266849962:web:675a64af83d57a6a"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+// creates a variable to reference the database
+var database = firebase.database();
+// Blank Values
+var name = "";
+var email = "";
+var message = "";
 
-    // (function($){
-    //     $(function(){
-      
-    //       $('#navbtn').sideNav({
-    //         edge: 'left', // Choose the horizontal origin
-    //         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    //         // draggable: true // Choose whether you can drag to open on touch screens
-    //       }
-    //       );
-    //     })
-    //     })
+// Capturing the Button Click
+$("#submitBtn").on("click", function(event) {
+    event.preventDefault();
+
+    // storing the data
+    name = $("#name").val().trim();
+    email = $("#email").val().trim();
+    message = $("#message").val().trim();
+
+    database.ref().set({
+        name: name,
+        email: email,
+        message: message
+    });
+
+    function cleartext() {
+        $("#name.materialize-textarea").value = "";
+        $("#email.materialize-textarea").empty = "";
+        $("#message").value = "";
+        console.log("Clicked");
+    };
+    cleartext();  
+
+});
+
+
+    
+
   });
